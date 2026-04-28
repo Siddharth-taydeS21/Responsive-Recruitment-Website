7f3ad89b1fe1d9915fe64d58075627ac48d8e435
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-04-28 16:45:11 +0530

    Add platfrom, reviews, FAQ, featured, cts & footer section.

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -8,7 +8,7 @@ toggleButton.addEventListener('click', () => {
 
 // ================= CLOSE MENU FOR MOBILE SCREENS =================
 const closeButton = document.querySelector('.nav_close');
-const hideMenu =  () => {
+const hideMenu = () => {
     navMenu.classList.toggle('max-lg:right-0');
     navMenu.classList.toggle('max-lg:right-[-120%]');
 }
@@ -25,7 +25,55 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     if (this.scrollY >= 50) {
         header.classList.add('shadow-lg');
-    }else{
+    } else {
         header.classList.remove('shadow-lg');
+    }
+})
+
+// ============= SWIPER JS ==================== 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 30,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+// ====================== SHOW FAQ ANSWERS ======================
+
+const accordians = document.querySelectorAll('.accordian');
+
+accordians.forEach(el => {
+    el.addEventListener('click', () => {
+        const answer = el.querySelector('.answer');
+        const icon = el.querySelector('i');
+
+        answer.classList.toggle('hidden');
+        icon.classList.toggle('rotate-0!');
+        icon.classList.toggle('rotate-45');
+
+        el.classList.toggle('bg-[var(--color-container-color)]!')
+        el.classList.toggle('bg-[var(--color-green-color-light)]!')
+    })
+})
+
+// ============= SCROLL UP BUTTON JS ==================== 
+
+const scrollUp = document.querySelector('.scrollUp');
+window.addEventListener('scroll', () => {
+    if(this.scrollY >= 350){
+        scrollUp.classList.remove('bottom-[-110%]');
+        scrollUp.classList.add('bottom-6');
+    }else{
+        scrollUp.classList.remove('bottom-6');
+        scrollUp.classList.add('bottom-[-110%]');
     }
 })
